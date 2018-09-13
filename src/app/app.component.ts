0d44c96ab94e6be78f591bdaf327149ba9acf086
Flora Calbris <flora.calbris@gmail.com>
2018-09-13 17:22:40 +0200

    maj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<button (click)="clicAime()">Clic</button>',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
